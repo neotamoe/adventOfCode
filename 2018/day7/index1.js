f913a5ba3a429a4f27406c1081f3535aa3e45ca7
Neota Moe <neota.moe@gmail.com>
2018-12-08 14:32:29 -0600

    more work, but still not working right

--- 2018/day7/index1.js
+++ 2018/day7/index1.js
@@ -1,8 +1,10 @@
 let fs = require('fs');
 
 let input = fs.readFileSync('./input.txt','utf8');
-
 let steps = input.split('\n');
+// let testinput = fs.readFileSync('./testinput.txt','utf8');
+// let steps = testinput.split('\n');
+
 
 for(let i=0; i<steps.length; i++){
     steps[i] = steps[i].replace('Step ', '').replace(' must be finished before step ', '').replace(' can begin.', '').split('');
@@ -23,7 +25,7 @@ let temp = "";
 while(k<26 && j<sortedByFirst.length){
     currentFirst = String.fromCharCode(65 + k);
     if(sortedByFirst[j][0]!==currentFirst){
-        temp = temp.split('').sort().join('');
+        temp = temp.split('').sort();
         map.set(currentFirst, temp);
         temp = "";
         k++;
@@ -34,28 +36,67 @@ while(k<26 && j<sortedByFirst.length){
     } 
 }
 
-map.set(currentFirst, temp);
+map.set(currentFirst, temp.split('').sort());
 
-// console.log(map);
+console.log(map);
 
 let finalString = "";
 let keys = map.keys();
-console.log(keys);
-let m = 0;
+let last = "";
 
-// while(finalString.length<26){
+for (var key of map.keys()) {
+    if(map.get(key).length==0){
+        finalString += key;
+        last = key;
+        console.log(key + ' is empty');
+        map.delete(key);
+    } 
+}
+console.log(map);
+console.log('found last letter: ' + last);
 
-    for (var key of map.keys()) {
-        if(map.get(key)==""){
-            finalString += key;
-            // console.log(key + ' is empty');
+let tempOrder = "";
+let minLength = 2;
+while(minLength>1){
+    for (key of map.keys()) {
+        let currLength = map.get(key).length;
+        if(currLength>minLength){
+            minLength = currLength;
         }
-        // console.log(key);
-        // console.log(map.get(String.fromCharCode(65 + m)))
-        m++;
-    }
+        if (map.get(key)==last && map.get(key).length===1 ){
+            tempOrder = tempOrder + key;
+            map.delete(key);
+        } 
+    }    
+    tempOrder = tempOrder.split('').sort().join('');
+    finalString = tempOrder + finalString;
+    last = finalString.charAt(0).toString();
+    console.log(map);
+    console.log('1st partial finalString:',finalString);
+    console.log('last',last);
+}
+
+
+// while(finalString.length<6){
+//     tempOrder = "";
+//     for (var key of map.keys()) {
+//         if (map.get(key).includes(last) ){
+//             tempOrder = tempOrder + key;
+//             map.delete(key);
+//             continue;
+//         } 
+//     }
+//     tempOrder = tempOrder.split('').sort().join('');
+//     finalString = tempOrder + finalString;
+//     last = finalString.charAt(0).toString();
+//     console.log(map);
+//     console.log('end partial finalString:',finalString);
+//     console.log('last',last);
 // }
 
+
+console.log(map)
 console.log(finalString);
+
 
 
