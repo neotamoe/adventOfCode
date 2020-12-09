0ea0220b2d58dc41e4df6b6ccf02c54c4acaa41c
neotamoe <neota.moe@gmail.com>
2020-12-08 21:33:38 -0600

    added part 2

--- 2020/day2/one.js
+++ 2020/day2/one.js
@@ -24,6 +24,25 @@ const findCorrectPasswords = function(line){
   return (count <= rulesMax && count >= rulesMin) ? 1 : 0
 }
 
+const findCorrectPasswordsTake2 = function(line){
+  let arr = line.split(':')
+  console.log(arr)
+  let rulesSplit = arr[0].split(' ')
+  let position1 = parseInt(rulesSplit[0].split('-')[0])
+  let position2 = parseInt(rulesSplit[0].split('-')[1])
+  let letter = rulesSplit[1]
+
+  let password = arr[1].trim()
+  let passwordArr = password.split('')
+  if((letter == passwordArr[position1-1] && letter == passwordArr[position2-1])){
+    return 0
+  }
+  if((letter == passwordArr[position1-1] || letter == passwordArr[position2-1])){
+    return 1
+  }
+  return 0
+}
+
 
 let inputs = input
   .split('\n')
@@ -31,3 +50,12 @@ let inputs = input
   .reduce((acc, curr) => acc + curr, 0)
 
 console.log(`final result: ${inputs}`)
+
+// console.log(findCorrectPasswordsTake2(inputs[0]))
+
+let inputs2 = input
+  .split('\n')
+  .map(item => findCorrectPasswordsTake2(item))
+  .reduce((acc, curr) => acc + curr, 0)
+
+console.log(`final result take 2: ${inputs2}`)
